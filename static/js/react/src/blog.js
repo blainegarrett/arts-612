7ede7fc6115bfedfff4080f352a7cbfafdb6f5da
Blaine Garrett <blaine@blainegarrett.com>
2014-12-21 13:05:06 -0600

    Working on Phase 3

--- static/js/react/src/blog.js
+++ static/js/react/src/blog.js
@@ -5,7 +5,7 @@
 
 var post_data = {
     'title': 'Jules Verne is Rad As Hell',
-    'content': 'Jive.. Some <b>uttjive</b> yo. toast<br />sfsdfsdf',
+    'content': ' It is with great pleasure that we introduce you to the new owners of mplsart.com, Katie and Blaine Garrett! Kristoffer and I have spent the past few months talking with them about our experience with the site, why it came to be, and our love of artists and this arts scene. We’ve talked about what makes the site and the scene work and what could make it better. From the first meeting, Katie and Blaine have voiced a true passion for digging in, supporting, and building upon what we\'ve started as a way to provide insight and guidance into this thing we call Minneapolis art. We were sold. We know they have the passion to make this work and we are happy to be handing over the keys. In the following months, they’ll have much to share. And we’ll be eagerly waiting to see what mplsart.com grows into. As Kristoffer and I step aside, we ask that you welcome Katie and Blaine and continue to support the galleries and artists of Minneapolis and beyond. ',
     'primary_image': 'https://scontent-b-ord.xx.fbcdn.net/hphotos-xap1/v/t1.0-9/10612934_10105497726369220_7905413968404678817_n.jpg?oh=1b353059e93e6b0714f2247a397ec20d&oe=550870EF',
     'author': { 'firstname': 'Captain', 'lastname': 'Kangaroo'},
     'created_date': 'August 24, 2013 9:00 PM'
@@ -22,27 +22,45 @@ var BlogArticlePage = React.createClass({
     },
 
     render: function () {
-        return <article>
-            <h2><a href="singlepost.html">{ this.state.post_data.title }</a></h2>
+        return <div>
+        <div class="row">
+      	<div class="col-md-12" id="welcome-header-container">
+    			<h1 id="welcome-header">
+    				GOOD NEWS, MINNEAPOLIS
+    			</h1>
+    		</div>
+    	</div>
+        <div className="row">
+            <div className="col-md-6">
+                <article>
+                    <h2><a href="singlepost.html">{ this.state.post_data.title }</a></h2>
 
-            <div className="row">
-                <div className="group1 col-md-12 well">
-                    <span className="glyphicon glyphicon-pencil"></span> by { this.state.post_data.author.firstname} { this.state.post_data.author.lastname}
-                    <br />                           
-                      <span className="glyphicon glyphicon-time"></span> August 24, 2013 9:00 PM
-                </div>
+                    <div className="row">
+                        <div className="group1 col-md-12 well">
+                            <span className="glyphicon glyphicon-pencil"></span> by { this.state.post_data.author.firstname} { this.state.post_data.author.lastname}
+                            <br />                           
+                              <span className="glyphicon glyphicon-time"></span> August 24, 2013 9:00 PM
+                        </div>
 
+                    </div>
+
+
+                    <img src={this.state.post_data.primary_image} className="img-responsive" />
+
+                    <br />
+
+                    <p className="lead" dangerouslySetInnerHTML={{__html: this.state.post_data.content}} />
+
+                    <hr />
+                </article>
             </div>
 
-
-            <img src={this.state.post_data.primary_image} className="img-responsive" />
-
-            <br />
-
-            <p className="lead" dangerouslySetInnerHTML={{__html: this.state.post_data.content}} />
-
-            <hr />
-        </article>
+    		<div className="col-md-3 panel-events"><TempUpcoming col_name="Upcoming" /></div>
+    		<div className="col-md-3 panel-events"><TempEvents col_name="Now Showing"  /></div>
+        </div>
+        
+        <footer id="main-content-footer"><p>We love art. We love you. &copy; 2014 mplsart</p></footer>
+        </div>;
     }
 });
 
