fe2034c3e62276e1d5697323788e5cd719bfdf8e
Blaine Garrett <blaine@blainegarrett.com>
2015-02-17 11:22:26 -0600

    Refactoring Events widgets to be closer to final version. Back porting this to the temporary widgets on the alpha site.

---
+++ app/public/src/utils/NiceDate.js
@@ -0,0 +1,52 @@
+var React = require('react');
+var moment = require('moment');
+
+var NiceDate = React.createClass({
+    propTypes: {
+        start: React.PropTypes.string,
+        end: React.PropTypes.string,
+        eventdate_type: React.PropTypes.string
+    },
+    getInitialState: function () {
+        return {
+            start: this.props.start,
+            end: this.props.end,
+            eventdate_type: this.props.eventdate_type
+        };
+    },
+    render: function () {
+        var start, end, display_str, duration_hours, start_time_format, end_time_format, extra_minute;
+
+        start = moment(Date.parse(this.state.start));
+        end = moment(Date.parse(this.state.end));
+
+        display_str = start.format("MMM Do") + ' - ' +  end.format("MMM Do");
+        duration_hours = end.diff(start, 'hours');
+
+        // this.state.eventdate_type == 'timed' might be a better indicator
+        if (duration_hours < 15) {
+            // Assumed to be a single timed event: Sat Nov 8th 7PM - 10:30PM
+
+            start_time_format = 'hA';
+            end_time_format = 'hA';
+
+            if (start.minutes() > 0) {
+                start_time_format = 'h:mmA';
+            }
+
+            if (end.minutes() > 0) {
+                end_time_format = 'h:mmA';
+            }
+
+            extra_minute = ':mm';
+
+            display_str = start.format("ddd MMM Do " + start_time_format);
+            display_str += ' - ';
+            display_str += end.format(end_time_format);
+        }
+
+        return <span>{ display_str }</span>;
+    }
+});
+
+module.exports = NiceDate;
