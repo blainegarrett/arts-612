6ca638f01eee5037c0200e4702e8786476ffff0b
Blaine Garrett <blaine@blainegarrett.com>
2015-07-21 19:26:46 -0500

    eliminating import tools

--- app/controllers/written.py
+++ app/controllers/written.py
@@ -10,7 +10,7 @@ from rest.params import coerce_to_cursor
 from controllers import BaseHandler
 from utils import get_domain
 
-from rest.utils import get_key_from_resource_id, get_resource_id_from_key
+from rest.utils import get_resource_id_from_key
 from rest.controllers import RestHandlerBase
 from rest.resource import Resource
 from rest.resource import RestField, SlugField, ResourceIdField
@@ -50,10 +50,10 @@ class WrittenMainHandler(BaseHandler):
 class WrittenArticleBaseHandler(BaseHandler):
     """
     """
-    
+
     def get_dereferenced_post(self, slug):
         """
-        Helper method to get a 
+        Helper method to get a
         """
         post = posts_api.get_post_by_slug(slug)
         if not post:
@@ -62,7 +62,6 @@ class WrittenArticleBaseHandler(BaseHandler):
         posts_api.bulk_dereference_posts([post])
         return post
 
-    
     def serve_up_article(self, post):
         """
         """
@@ -86,12 +85,11 @@ class WrittenArticleBaseHandler(BaseHandler):
         self.render_template('./templates/index.html', template_values)
 
 
-
 class WrittenCategoryArticleHandler(WrittenArticleBaseHandler):
     """
     Written Article Web Handler
     """
-    
+
     def get(self, category_slug, slug):
         post = self.get_dereferenced_post(slug)
         if not post:
@@ -227,7 +225,7 @@ class PostCategoriesApiHandler(RestHandlerBase):
         return CATEGORY_REST_RULES
 
     def get_param_schema(self):
-        
+
         return {
             'limit': voluptuous.Coerce(int),
             'cursor': coerce_to_cursor,
@@ -237,7 +235,7 @@ class PostCategoriesApiHandler(RestHandlerBase):
             #'q': voluptuous.Coerce(str)
         }
     def _get(self):
-        
+
         get_by_slug = self.cleaned_params.get('get_by_slug', None)
 
         if get_by_slug:
@@ -249,7 +247,7 @@ class PostCategoriesApiHandler(RestHandlerBase):
             else:
                 self.serve_success(Resource(category, CATEGORY_REST_RULES).to_dict())
                 return
-        
+
         # Refactor this...
         results = []
         entities = BlogCategory.query().fetch(1000)
@@ -293,18 +291,16 @@ class PostsApiHandler(RestHandlerBase):
     """
 
     def get_param_schema(self):
-        
-        #raise Exception(voluptuous.Coerce(voluptuous.Boolean())('true'))
-        
+
         return {
             u'limit': voluptuous.Coerce(int),
             u'cursor': coerce_to_cursor,
-            #'sort': voluptuous.Coerce(str),
+            # 'sort': voluptuous.Coerce(str),
             u'category_slug': voluptuous.Coerce(unicode),
             u'get_by_slug': voluptuous.Coerce(unicode),
             u'is_published': voluptuous.Coerce(voluptuous.Boolean()),
-            u'start_date': coerce_to_datetime 
-            #'q': voluptuous.Coerce(str)
+            u'start_date': coerce_to_datetime
+            # 'q': voluptuous.Coerce(str)
         }
 
     def get_rules(self):
@@ -324,7 +320,7 @@ class PostsApiHandler(RestHandlerBase):
         # Found Post: Serve it up
         self.serve_success(Resource(post, REST_RULES).to_dict())
         return
-    
+
     def _get(self):
         """
         Get a collection of Written Posts
@@ -350,7 +346,7 @@ class PostsApiHandler(RestHandlerBase):
 
         if start_date:
             optional_params['start_date'] = start_date
-        
+
         category_resource_id_filter = None
         if category_slug:
             category = posts_api.get_post_category_by_slug(category_slug)
@@ -429,183 +425,3 @@ class PostDetailApiHandler(RestHandlerBase):
         post = posts_api.edit_post(post, self.cleaned_data)
 
         self.serve_success(Resource(post, REST_RULES).to_dict())
-
-
-class ImportWPHandler(BaseHandler):
-    total = 0
-    author_map = {}
-    category_map = {}
-    author_totals = {}
-    img_urls = []
-
-    def process_post_content(self, content):
-        import re
-        content = content.replace(u'\xc2\xa0', ' ')
-        
-        image_urls = re.findall("(http://www.mplsart.com/written/wp-content/uploads/[^\"]*)\"", content, re.MULTILINE)
-        if image_urls:
-            self.img_urls.extend(image_urls)
-            #self.img_urls.extend(image_urls.groups())
-            #raise Exception(image_urls.groups())
-            #self.response.write(image_urls)
-
-        content = content.replace('http://www.mplsart.com/written/wp-content/uploads/', 'http://cdn.mplsart.com/written/wp-content/uploads/')
-        content = content.replace('http://www.mplsart.com/blog/blog_pics/', 'http://cdn.mplsart.com/written/wp-content/uploads/blog_pics/')
-        content = content.replace('http://mplsart.com/blog/blog_pics/', 'http://cdn.mplsart.com/written/wp-content/uploads/blog_pics/')
-        content = content.replace('http://mplsart.com/images/', 'http://cdn.mplsart.com/written/wp-content/uploads/images/')
-        content = content.replace('http://www.mplsart.com/images/', 'http://cdn.mplsart.com/written/wp-content/uploads/images/')
-
-        content = content.replace('\n', '<br />')
-        return u"<p>%s</p>" % content
-        
-    def migrate_post(self, post_data):
-        import datetime
-        
-        # Filter out stuff we don't want to migrate...
-
-        if post_data.get('post_type') not in ('post', 'page'):
-            return
-        
-        if post_data.get('post_status') == 'draft':
-            return
-        
-        # Skip posts by slug
-        if post_data.get('post_name') in ('blaine-test-post', 'critics', '66', '43'):
-            return
-
-        post_password = post_data.get('post_password')
-        comment_status = post_data.get('comment_status')
-        post_excerpt = post_data.get('post_excerpt')
-        post_content = post_data.get('post_content')
-        post_modified_gmt = post_data.get('post_modified_gmt')
-        post_date_gmt = post_data.get('post_date_gmt')
-        post_category = post_data.get('post_category')
-        post_parent = post_data.get('post_parent')
-        post_author = post_data.get('post_author')
-        post_date = post_data.get('post_date')
-        to_ping = post_data.get('to_ping')
-        post_status = post_data.get('post_status')
-        post_title = post_data.get('post_title')
-        post_modified = post_data.get('post_modified')
-        guid = post_data.get('guid')
-        post_name = post_data.get('post_name')
-        ping_status = post_data.get('ping_status')
-        post_mime_type = post_data.get('post_mime_type')
-        ID = post_data.get('ID')
-        menu_order = post_data.get('menu_order')
-        pinged = post_data.get('pinged')
-        post_type = post_data.get('post_type')
-        post_content_filtered = post_data.get('post_content_filtered')
-        comment_count = post_data.get('comment_count')
-
-        
-        self.response.write( post_type + ') - <a href="/written/2009/02/' +  post_name + '" target="_new">' + post_name + '</a> ... (cat: ' + post_category + ', parent: ' + post_parent + ') ' + post_author + '<br />')
-
-        self.author_totals[str(post_author)] = int(self.author_totals.get(str(post_author), 0)) + 1
-        self.total += 1
-
-        # REST PAYLOAD
-
-        category_resource_id = None
-
-        if post_parent and not post_parent == u'0':
-            if int(post_parent) == 175:
-                category_resource_id = self.author_map['exhibition-reviews']
-            else:
-                category_resource_id = self.author_map['art-on-the-wall']
-        else:
-            category_resource_id = 'QmxvZ0NhdGVnb3J5Hh81NzYxNjczOTMxNTIyMDQ4' # Blog
-
-
-        post_content = self.process_post_content(post_content)
-
-        payload = {
-            'slug': post_name,
-            'title': post_title,
-            'content': post_content,
-            'summary': '',
-            'published_date': datetime.datetime.strptime(post_date_gmt, '%Y-%m-%d %H:%M:%S'),
-            'is_published': True,
-            'author_resource_id': self.author_map.get(str(post_author)),
-            'category_resource_id': category_resource_id
-        }
-
-        post = posts_api.create_post(payload)
-
-    
-    def get(self):
-        raise Exception('Import is disabled for now.')
-        
-        # Step 1: Create Author records (this is for local use only - these exist on prod)
-        from auth.api import create_user
-        from rest.utils import get_key_from_resource_id
-
-        author_data = [
-            {'old_id': None, 'key': 'VXNlch4fNTE4NDU4MTg1ODc1NDU2MA', 'firstname': 'Katie',  'lastname': 'Garrett', 'website': 'http://mplsart.com'},
-            {'old_id': None, 'key': 'VXNlch4fNTY3MDQwNTg3NjQ4MjA0OA', 'firstname': 'Blaine',  'lastname': 'Garrett', 'website': 'http://mplsart.com'},
-            {'old_id': 2, 'key': 'VXNlch4fNTc2NzQwOTU5MTkxMDQwMA', 'firstname': 'Emma',  'lastname': 'Berg', 'website': 'http://mplsart.com'},
-            {'old_id': 4, 'key': 'VXNlch4fNTY5NjQ1OTE0ODA5OTU4NA', 'firstname': 'Kristoffer',  'lastname': 'Knutson', 'website': 'http://mplsart.com'},
-            {'old_id': 37, 'key': 'VXNlch4fNTc1OTgyNjI5MDI3ODQwMA', 'firstname': 'John',  'lastname': 'Megas', 'website': 'http://mplsart.com'},
-            {'old_id': 38, 'key': 'VXNlch4fNTY5NjYwNTcxMzg1ODU2MA', 'firstname': 'Tristan',  'lastname': 'Pollock', 'website': 'http://mplsart.com'},
-            {'old_id': 14, 'key': 'VXNlch4fNTc1MTM5OTgzMjg3OTEwNA', 'firstname': 'tiny',  'lastname': 'shoes', 'website': 'http://mplsart.com'},
-            {'old_id': None, 'key': 'VXNlch4fNTcwMDg2NjA1Mjk4MDczNg', 'firstname': 'Melissa',  'lastname': 'Stang', 'website': 'http://mplsart.com'},
-            {'old_id': None, 'key': 'VXNlch4fNTczMDQ1MDA1NjE1MTA0MA', 'firstname': 'Maddy',  'lastname': 'Huges', 'website': 'http://mplsart.com'},
-        ]
-
-        category_data = [
-            {'old_ids': [12,10], 'key': 'QmxvZ0NhdGVnb3J5Hh81NjU4MDkxNjY4MzczNTA0', 'title': 'MPLSART', 'slug': 'mplsart'},
-            {'old_ids': [], 'key': 'QmxvZ0NhdGVnb3J5Hh81NjczMzA5NTQyODA5NjAw', 'title': 'Art On The Wall', 'slug': 'art-on-the-wall'},
-            {'old_ids': [3], 'key': 'QmxvZ0NhdGVnb3J5Hh81NzI4MTE2Mjc4Mjk2NTc2', 'title': 'Exhibition Reviews', 'slug': 'exhibition-reviews'},
-            {'old_ids': [9, 11], 'key': 'QmxvZ0NhdGVnb3J5Hh81NzYxNjczOTMxNTIyMDQ4', 'title': 'Blog', 'slug': 'blog'},
-            {'old_ids': [7,4, 10, 6, 8], 'key': 'QmxvZ0NhdGVnb3J5Hh81NzYzMjYzNjA2MjkyNDgw', 'title': 'The Scene', 'slug': 'scene'}
-        ]
-
-        # Populate Map and Construct Entities
-        self.author_map['art-on-the-wall'] = 'QmxvZ0NhdGVnb3J5Hh81NjczMzA5NTQyODA5NjAw'
-        self.author_map['exhibition-reviews'] = 'QmxvZ0NhdGVnb3J5Hh81NzI4MTE2Mjc4Mjk2NTc2'
-
-        for user_data in author_data:
-            old_id = user_data.pop('old_id')
-            user_resource_id = user_data['key']
-            user_data['key'] = get_key_from_resource_id(user_resource_id)
-            #create_user(user_data)
-            
-            if old_id:
-                self.author_map[str(old_id)] = user_resource_id
-
-        # Populate Map and Construct Entities
-        for category_data in category_data:
-            old_ids = category_data.pop('old_ids')
-            category_resource_id = category_data['key']
-            category_data['key'] = get_key_from_resource_id(category_resource_id)
-
-            try:
-                pass
-                #posts_api.create_post_category(category_data)
-            except:
-                pass
-
-            if old_ids:
-                for old_id in old_ids:
-                    self.category_map[str(old_id)] = category_resource_id
-
-
-        # Step 2: Dump existing migrated data
-
-        #posts = BlogPost.query().fetch(1000)
-        #for p in posts:
-        #    p.key.delete()
-        
-
-        # Step 3: Read in new data
-        import json
-        data = open('./controllers/wp_posts.json', 'r').read()
-        j_data = json.loads(data)
-        
-        for post_data in j_data:
-            self.migrate_post(post_data)
-        
-        self.response.write(self.total)
-        self.response.write(self.author_totals)
-        self.response.write(self.img_urls)
-
