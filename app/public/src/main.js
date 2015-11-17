011c8ec4f1958b9fea1f51702dce1cf3708738f2
Blaine F Garrett <blaine@blainegarrett.com>
2015-11-16 21:10:08 -0600

    Refactoring app to have a single root parent.

--- app/public/src/main.js
+++ app/public/src/main.js
@@ -20,56 +20,179 @@ var PrimaryMenu = require('./components/pages/PrimaryMenu')
 
 var analytics = require('./utils/analytics');
 
+
+var SlideMenu = React.createClass({
+
+    render: function() {
+        return (
+            <div id="site-menu">
+                <ul className="list-unstyled main-menu">
+                    <li><a href="/" data-ga-category="menu-link" className="internal-link">EVENTS<span className="icon"></span></a></li>
+                    <li><a href="/about/" data-ga-category="menu-link" className="internal-link">ABOUT<span className="icon"></span></a></li>
+                    <li><a href="/written/" data-ga-category="menu-link" className="internal-link">WRITTEN<span className="icon"></span></a></li>
+                    <li><a href="mailto:calendar@mplsart.com" data-ga-category="menu-link">calendar@mplsart.com<span className="icon"></span></a></li>
+                </ul>
+
+                <p className="social-icons">
+                    <a href="https://www.facebook.com/mplsart" target="_new">
+                        <i className="fa fa-facebook"></i>
+                    </a>
+
+                    <a href="https://twitter.com/mplsart" target="_new">
+                        <i className="fa fa-twitter"></i>
+                    </a>
+
+                    <a href="http://instagram.com/mplsart" target="_new">
+                        <i className="fa fa-instagram"></i>
+                    </a>
+                </p>
+
+                <p>Est. 2005<br />
+                    We love art. We love you.<br />
+                    &copy; 2015 MPLSART.COM
+                </p>
+            </div>
+
+        );
+    }
+});
+
+
+var HeaderNav = React.createClass({
+    /* TODO: Bind this up to a store and event */
+
+    /* Code For Sliding Navs and Featured Widget */
+    closeMenu: function () {
+        // Do things on Nav Close
+        $('body').removeClass('show-menu');
+        $(".modal-backdrop").remove();
+
+        // Emit event that menu closed
+    },
+
+    openMenu: function () {
+        // Do things on Nav Open
+        $('body').addClass('show-menu');
+        var modal = $('<div class="modal-backdrop"></div>');
+
+        modal.bind('click tap', this.closeMenu)
+        modal.appendTo(document.body);
+
+        // Emit event that menu opened
+    },
+
+    toggleNav: function (e) {
+
+        var ga_category = 'menu-toggle';
+        var ga_action = e.type;
+        var ga_label;
+
+        if ($('body').hasClass('show-menu')) {
+            this.closeMenu();
+            ga_label = 'close';
+        }
+        else {
+            this.openMenu()
+            ga_label = 'open';
+        }
+
+        analytics.record_event(ga_category, ga_action, ga_label, 1);
+    },
+
+    render: function () {
+        return (
+            <div id="header_nav" className="navbar navbar-fixed-top navbar-inverse" role="navigation">
+                <div className="container">
+                    <div className="navbar-header">
+                        <a id="side_nav_toggle" className="pull-left" title="Show Menu" onClick={ this.toggleNav }><span></span></a>
+                        <a className="navbar-brand internal-link" href="/" title="MPLSART.COM Home Page">MPLSART.COM</a>
+                    </div>
+                    <p className="navbar-text navbar-right"></p>
+                </div>
+                <div id="header_nav_cards"><NavCardsContainer /></div>
+            </div>
+        );
+    }
+});
+
+
+
+var App = React.createClass({
+
+    render: function(){
+        return (
+            <div>
+                <SlideMenu />
+                <HeaderNav />
+
+                <div id="site-wrapper">
+                    <div id="site-canvas">
+                        <div className="container">
+                            <div id="main_content">
+                                { this.props.children }
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>);
+    }
+});
+
+
+
 global.current_page = null; // This is set by PageMixin to the current page for use in widgets
 
+
+var target_e = document.getElementById('root');
 /* Figure out what to do with Routes... This is fugly... */
 ReactRouter.createRoute('/', function () {
-    React.unmountComponentAtNode(document.getElementById('main_content'));
-    React.render(<NewHomePage />, document.getElementById('main_content'));
+    React.unmountComponentAtNode(target_e);
+    React.render(<App><NewHomePage /></App>, target_e);
 });
 
 
 ReactRouter.createRoute('/about/', function () {
-    React.unmountComponentAtNode( document.getElementById('main_content'));
-    React.render(<AboutPage />, document.getElementById('main_content'));
+    React.unmountComponentAtNode( target_e);
+    React.render(<AboutPage />, target_e);
 });
 
 
 ReactRouter.createRoute('/events/{slug}/', function (params) {
-    React.unmountComponentAtNode( document.getElementById('main_content'));
-    React.render(<EventPage slug={params.slug} />, document.getElementById('main_content'));
+    React.unmountComponentAtNode( target_e);
+    React.render(<EventPage slug={params.slug} />, target_e);
 });
 
 ReactRouter.createRoute('/calendar/', function () {
-    React.unmountComponentAtNode( document.getElementById('main_content'));
-    React.render(<CalendarPage/>, document.getElementById('main_content'));
+    React.unmountComponentAtNode( target_e);
+    React.render(<CalendarPage/>, target_e);
 });
 
 ReactRouter.createRoute('/galleries/', function () {
-    React.unmountComponentAtNode( document.getElementById('main_content'));
-    React.render(<GalleryPages.GalleryIndexPage />, document.getElementById('main_content'));
+    React.unmountComponentAtNode( target_e);
+    React.render(<GalleryPages.GalleryIndexPage />, target_e);
 });
 
 ReactRouter.createRoute('/galleries/{slug}/', function (params) {
-    React.unmountComponentAtNode( document.getElementById('main_content'));
-    React.render(<GalleryPages.GalleryViewPage slug={params.slug} />, document.getElementById('main_content'));
+    React.unmountComponentAtNode( target_e);
+    React.render(<GalleryPages.GalleryViewPage slug={params.slug} />, target_e);
 });
 
 ReactRouter.createRoute('/written/', function () {
-    React.unmountComponentAtNode( document.getElementById('main_content'));
-    React.render(<WrittenPage />, document.getElementById('main_content'));
+    React.unmountComponentAtNode( target_e);
+    React.render(<WrittenPage />, target_e);
 });
 
 
 ReactRouter.createRoute('/written/{category_slug}/', function (params) {
-    React.unmountComponentAtNode( document.getElementById('main_content'));
-    React.render(<WrittenCategoryPage category_slug={params.category_slug} />, document.getElementById('main_content'));
+    React.unmountComponentAtNode( target_e);
+    React.render(<WrittenCategoryPage category_slug={params.category_slug} />, target_e);
 });
 
 
 ReactRouter.createRoute('/written/{category_slug}/{slug}/', function (params) {
-    React.unmountComponentAtNode( document.getElementById('main_content'));
-    React.render(<WrittenArticlePage category_slug={params.category_slug} slug={params.slug} />, document.getElementById('main_content'));
+    React.unmountComponentAtNode( target_e);
+    React.render(<WrittenArticlePage category_slug={params.category_slug} slug={params.slug} />, target_e);
 });
 
 
@@ -77,13 +200,13 @@ ReactRouter.createRoute('/written/{category_slug}/{slug}/', function (params) {
 
 
 ReactRouter.createRoute('/written/{year}/{month}/{slug}/', function (params) {
-    React.unmountComponentAtNode( document.getElementById('main_content'));
-    React.render(<WrittenArticlePage year={params.year} month={params.month} slug={params.slug} />, document.getElementById('main_content'));
+    React.unmountComponentAtNode( target_e);
+    React.render(<WrittenArticlePage year={params.year} month={params.month} slug={params.slug} />, target_e);
 });
 
 ReactRouter.createRoute('*', function () {
-    React.unmountComponentAtNode( document.getElementById('main_content'));
-    React.render(<Error404Page />, document.getElementById('main_content'));
+    React.unmountComponentAtNode( target_e);
+    React.render(<Error404Page />, target_e);
 });
 
 ReactRouter.init();
@@ -93,39 +216,7 @@ global.targed_tonight_end_date = moment().hour(9).minute(0).second(0);
 global.targed_tonight_end_date = moment.utc(global.targed_tonight_end_date);
 
 
-/* Code For Sliding Navs and Featured Widget */
-global.closeMenu = function () {
-    // Do things on Nav Close
-    $('body').removeClass('show-menu');
-	$(".modal-backdrop").remove();
-}
 
-global.openMenu = function () {
-    // Do things on Nav Open
-    $('body').addClass('show-menu');
-	var modal = $('<div class="modal-backdrop"></div>');
-
-	modal.bind('click tap', closeMenu)
-	modal.appendTo(document.body);
-}
-
-global.toggleNav = function (e) {
-
-    var ga_category = 'menu-toggle';
-    var ga_action = e.type;
-    var ga_label;
-
-    if ($('body').hasClass('show-menu')) {
-        closeMenu();
-        ga_label = 'close';
-    }
-    else {
-        openMenu()
-        ga_label = 'open';
-    }
-
-    analytics.record_event(ga_category, ga_action, ga_label, 1);
-}
 
 
 // navigateTo
@@ -136,10 +227,10 @@ $(function() {
 
 	$('#site-menu').bind('swiperight', function(e) { toggleNav(e) });
 
-	$('#side_nav_toggle').click(function(e) {
-		// Calling a function in case you want to expand upon this.
-		toggleNav(e);
-	});
+	//$('#side_nav_toggle').click(function(e) {
+	//	// Calling a function in case you want to expand upon this.
+	//	toggleNav(e);
+	//});
 
     // Important: Including 'tap' here will trigger both events and cause routing to dble load
     //  and cause invarient react errors. Also, page load analytics record twice.
@@ -210,6 +301,3 @@ $(window).on("scroll touchmove", function () {
 
 /* Components outside of the main_content area*/
 React.render(<PageMeta />, document.getElementById('page_meta'));
-//React.render(<PageMeta />, document.getElementById('meta_debug'));
-//React.render(<PrimaryMenu />, document.getElementById('navbar'));
-React.render(<NavCardsContainer />, document.getElementById('header_nav_cards'));
